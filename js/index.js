6c5d587901e34d61535d6ccff9e40260daa44827
Javier B <javierboscan16@gmail.com>
2022-02-12 17:25:36 -0400

    joining colaborators code

--- js/index.js
+++ js/index.js
@@ -6,3 +6,13 @@ window.onload = () => {
 
 	if ($charging.classList.contains('hidden')) $charging.classList.add('none');
 };
+
+document.addEventListener('click', (event) => {
+	const $btnMenu = document.querySelector('#icon__home'),
+				$navbar = document.querySelector('.header__nav-container');
+
+	if (event.target === $btnMenu) {
+		$navbar.classList.toggle('show__menu');
+	}
+
+})
