1d62567864f2cbcac10b42227cfe56efe870a5f2
adriDiazz <adriandiazmanzanares9@gmail.com>
2022-02-12 03:05:45 +0100

    Time out charger added

---
+++ js/charging.js
@@ -0,0 +1,17 @@
+
+    // $(document).ready(function() {
+    //     $(".jm-loadingpage").fadeOut("slow");;
+
+    
+    $('.channel-yt__video').ready(function() {
+        setTimeout(() => {
+            $('.charging').fadeOut("slow");
+          }, 2000);
+        
+    })
+
+    // $(document).ready(function() {
+    //     $(".jm-loadingpage").fadeOut("slow");;
+    // });
+
+
